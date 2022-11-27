1bcccaa37f9fcf1a4ef8c5cee464338c1e50bd44
IrisDevelopment <75667602+IrisDevelopment@users.noreply.github.com>
2022-11-27 19:51:46 +1100

    Update 2788229376.lua

--- GamePatches/2788229376.lua
+++ GamePatches/2788229376.lua
@@ -5,7 +5,7 @@
 ]]
 
 -- // Dependencies
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/Module.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/IrisDevelopment/Aiming/main/Module.lua"))()
 
 -- // Disable Team Check
 local AimingIgnored = Aiming.Ignored
